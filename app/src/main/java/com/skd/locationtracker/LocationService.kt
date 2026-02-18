547637d852df60c18cfe8910a594ae49d1bfaf5a
saruj <14skd12@gmail.com>
2026-02-18 23:08:15 +0530

    added new in service file

--- app/src/main/java/com/skd/locationtracker/LocationService.kt
+++ app/src/main/java/com/skd/locationtracker/LocationService.kt
@@ -70,6 +70,7 @@ class LocationService : Service() {
         fusedClient.removeLocationUpdates(locationCallback)
         super.onDestroy()
     }
+
     override fun onBind(intent: Intent?): IBinder? = null
     private fun createNotification(): Notification {
         val channelId = "location_channel"
